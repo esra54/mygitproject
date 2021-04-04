b9ccc1984b8532ddf329de069d69d73c83e25018
esra54 <61192911+esra54@users.noreply.github.com>
2021-04-04 12:03:05 -0400

    Update MyGitClass.java

--- src/mygitproject/MyGitClass.java
+++ src/mygitproject/MyGitClass.java
@@ -6,7 +6,7 @@ public class MyGitClass {
 	System.out.println("My second commit");
 	System.out.println("My third commit");	//bunu remoteda yaptim.
 	System.out.println("My fouth commit");	//bunu remoteda yaptim
-		
+	System.out.println("My takimarkadasi commit");	//takim arkadasim yazdi
 	}
 
 }
